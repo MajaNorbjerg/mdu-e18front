d9a4eda5ff2fa575c604d7b479c1660d6a2a2cfb
Rasmus Cederdorff <rasmus@cederdorff.com>
2019-08-01 09:56:07 +0200

    Project template added.

---
+++ project_template/js/main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// appends string to the DOM
+document.querySelector("#content").innerHTML = "Hi Frontenders!";
+
+// declaring techer objects
+let teacher1 = {
+  name: 'Birgitte',
+  initials: 'bki'
+};
+
+let teacher2 = {
+  name: 'Gertie',
+  initials: 'gkj'
+};
+
+let teacher3 = {
+  name: 'Kim',
+  initials: 'kije'
+};
+
+let teacher4 = {
+  name: 'Rasmus',
+  initials: 'race'
+};
+
+// logs the teacher objects to the console
+console.log(teacher1);
+console.log(teacher2);
+console.log(teacher3);
+console.log(teacher4);
