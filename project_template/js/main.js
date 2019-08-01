2d873a1400d70330837378bbb3e5738b3455532e
Rasmus Cederdorff <rasmus@cederdorff.com>
2019-08-01 13:07:52 +0200

    project_template updated

--- project_template/js/main.js
+++ project_template/js/main.js
@@ -14,18 +14,7 @@ let teacher2 = {
   initials: 'gkj'
 };
 
-let teacher3 = {
-  name: 'Kim',
-  initials: 'kije'
-};
-
-let teacher4 = {
-  name: 'Rasmus',
-  initials: 'race'
-};
 
 // logs the teacher objects to the console
 console.log(teacher1);
 console.log(teacher2);
-console.log(teacher3);
-console.log(teacher4);
