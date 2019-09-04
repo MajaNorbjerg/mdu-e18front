8ccdf9bb986f646616b0b149f80276e31c635d5b
Rasmus Cederdorff <rasmus@cederdorff.com>
2019-09-04 10:49:45 +0200

    Templated updated

--- materialize_template/js/main.js
+++ materialize_template/js/main.js
@@ -1,9 +1,1 @@
 "use strict";
-
-document.addEventListener('DOMContentLoaded', function() {
-  let parallaxElems = document.querySelectorAll('.parallax');
-  let parallaxInstances = M.Parallax.init(parallaxElems);
-
-  let sidenavElems = document.querySelectorAll('.sidenav');
-  let sidenavInstances = M.Sidenav.init(sidenavElems);
-});
